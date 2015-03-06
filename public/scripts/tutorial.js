708e062da5ac9b67239480520c25f513a86413c5
Naoki Endoh <naoki.endoh@gmail.com>
2015-03-06 19:55:20 +0900

    Tutorial: Reactive state

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,8 +1,3 @@
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var converter = new Showdown.converter();
 
 var Comment = React.createClass({
@@ -20,11 +15,30 @@ var Comment = React.createClass({
 });
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -59,6 +73,6 @@ var CommentForm = React.createClass({
 });
 
 React.render(
-  <CommentBox data={data} />,
+  <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
