1cc68c2f7c43dd9183c9bd72dd591ae284bb5080
Naoki Endoh <naoki.endoh@gmail.com>
2015-03-12 19:56:38 +0900

    Tutorial with browserify: Adding markdown

--- public/scripts/tutorial2.js
+++ public/scripts/tutorial2.js
@@ -1,13 +1,25 @@
 var React = require('react');
+var marked = require('marked');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 var Comment = React.createClass({
   render: function() {
+    var rawMarkup = marked(this.props.children.toString());
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
